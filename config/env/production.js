a6a43c6142f6616a1bc7db55d4f4fb6ea1fa6315
Matthew Bell <matthew.bell@gettyimages.com>
2020-10-30 09:22:53 -0700

    updating example.com to be the actual host for prod

--- config/env/production.js
+++ config/env/production.js
@@ -374,7 +374,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'http://boiling-ocean-86329.herokuapp.com/',
     internalEmailAddress: 'support@example.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
